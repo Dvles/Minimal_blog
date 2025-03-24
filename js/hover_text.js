0d044b4c6366f011a3aa7f8c4889e033e50b813a
Dvles <132080646+Dvles@users.noreply.github.com>
2025-03-24 13:32:12 +0100

    Enhance UX with hover text effect

---
+++ js/hover_text.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const btn = document.getElementById("hover-btn");
+    btn.addEventListener("mouseenter", () => {
+        btn.innerText = btn.getAttribute("data-hover");
+    });
+    btn.addEventListener("mouseleave", () => {
+        btn.innerText = btn.getAttribute("data-default");
+    });
+});
